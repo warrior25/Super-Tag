65cbf251bdf0d096060cf5d9438ff06eb6340d40
NurmiJJ <nurmim@iki.fi>
2024-05-08 18:32:39 +0300

    Add recyclerview for players in lobby

--- app/src/main/java/com/huikka/supertag/LobbyActivity.kt
+++ app/src/main/java/com/huikka/supertag/LobbyActivity.kt
@@ -1,18 +1,25 @@
 package com.huikka.supertag
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.huikka.supertag.data.AuthDao
 import com.huikka.supertag.data.GameDao
+import com.huikka.supertag.data.model.Player
 
 class LobbyActivity : AppCompatActivity() {
 
     private val db = GameDao()
     private val auth = AuthDao()
     private lateinit var gameId: String
+
+    private var players: ArrayList<Player> = ArrayList()
+    private val adapter = PlayerListAdapter(players, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +32,21 @@ class LobbyActivity : AppCompatActivity() {
         }
 
         gameId = intent.getStringExtra("GAME_ID")!!
+        val gameIdView = findViewById<TextView>(R.id.game_id)
+        gameIdView.text = gameId
+
+        // Just for testing until players are fetched from DB
+        players.add(Player(id = "1", name = "Player 1"))
+        players.add(Player(id = "2", name = "Player 2"))
+        players.add(Player(id = "3", name = "Player 3"))
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.adapter = adapter
+
+        val randomButton = findViewById<Button>(R.id.pick_random)
+        randomButton.setOnClickListener {
+            adapter.selectRandom();
+        }
     }
 
     private suspend fun leaveGame() {
---
+++ app/src/main/java/com/huikka/supertag/playerListAdapter.kt
@@ -0,0 +1,84 @@
+package com.huikka.supertag
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.huikka.supertag.data.model.Player
+import kotlin.random.Random
+
+class PlayerListAdapter(private val playerList: ArrayList<Player>, private val context: Context) :
+    ListAdapter<Player, PlayerListAdapter.PlayerViewHolder>(PlayerComparator()) {
+
+    private var checkedPosition: Int = 0;
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerViewHolder {
+        return PlayerViewHolder.create(parent)
+    }
+
+    /**
+     * Creates possibility to click recyclerView item
+     */
+    override fun onBindViewHolder(holder: PlayerViewHolder, position: Int) {
+        val current = playerList[position]
+        holder.bind(current, checkedPosition)
+
+        holder.itemView.setOnClickListener {
+            val oldPosition = checkedPosition
+            checkedPosition = holder.adapterPosition
+
+            notifyItemChanged(checkedPosition)
+            notifyItemChanged(oldPosition)
+        }
+    }
+
+    /**
+     * Binds journey data to one recyclerView line
+     */
+    class PlayerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val playerName: TextView = itemView.findViewById(R.id.playerName)
+
+        fun bind(player: Player, checkedPosition: Int) {
+            playerName.text = player.name
+            if (checkedPosition == adapterPosition) {
+                playerName.setBackgroundResource(R.color.blue)
+            } else {
+                playerName.setBackgroundResource(R.color.white)
+            }
+
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): PlayerViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+                return PlayerViewHolder(view)
+            }
+        }
+    }
+
+    override fun getItemCount() = playerList.size
+
+    fun selectRandom() {
+        val oldPosition = checkedPosition
+        checkedPosition = Random.nextInt(0, getItemCount())
+
+        notifyItemChanged(checkedPosition)
+        notifyItemChanged(oldPosition)
+    }
+
+    class PlayerComparator : DiffUtil.ItemCallback<Player>() {
+        override fun areItemsTheSame(oldItem: Player, newItem: Player): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Player, newItem: Player): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+}
