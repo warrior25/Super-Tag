ca9093e49cf0f46aed3f025b2228272baa72e9a8
NurmiJJ <nurmim@iki.fi>
2024-05-17 17:22:21 +0300

    Add custom timer

--- app/src/main/java/com/huikka/supertag/fragments/ChaserTimers.kt
+++ app/src/main/java/com/huikka/supertag/fragments/ChaserTimers.kt
@@ -1,7 +1,36 @@
 package com.huikka.supertag.fragments
 
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.huikka.supertag.CustomTimer
 import com.huikka.supertag.R
 
 class ChaserTimers : Fragment(R.layout.chaser_timers) {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.chaser_timers, container, false)
+
+        val customItemView = view.findViewById<CustomTimer>(R.id.testTimer)
+        customItemView.setOnClickListener {
+            onItemClick("timer")
+            // Handle click event here if needed
+        }
+
+        return view
+    }
+
+    private fun onItemClick(text: String) {
+        Toast.makeText(requireContext(), "Clicked: $text", Toast.LENGTH_SHORT).show()
+    }
+
 }
