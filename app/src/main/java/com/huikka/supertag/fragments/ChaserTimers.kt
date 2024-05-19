ae0e911d70481c288a6e68d7499d9fe40e5646ea
warrior25 <eetu.hakkinen@hotmail.com>
2024-05-19 14:21:29 +0300

    Remove duplicate dependencies and bump minSdk to 26

--- app/src/main/java/com/huikka/supertag/fragments/ChaserTimers.kt
+++ app/src/main/java/com/huikka/supertag/fragments/ChaserTimers.kt
@@ -1,22 +1,18 @@
 package com.huikka.supertag.fragments
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.huikka.supertag.CustomTimer
 import com.huikka.supertag.R
 
 class ChaserTimers : Fragment(R.layout.chaser_timers) {
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.chaser_timers, container, false)
 
